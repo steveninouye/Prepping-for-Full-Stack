a74d2d02a7de4313d39e529bd154b7329ccae3a3
Steven Inouye <steveninouye@msn.com>
2018-08-22 20:27:32 -1000

    set up directories, files, webpack, and package.json

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import { join } from 'path';
+import express from 'express';
+import routes from './routes/index';
+// import stateRouting from './middleware/routing.mw'
+
+const CLIENT_PATH = join(__dirname, '../../client');
+
+const app = express();
+
+app.use(express.static(CLIENT_PATH));
+app.use(express.json());
+
+app.use('/api', routes);
+
+// app.use(stateRouting);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
