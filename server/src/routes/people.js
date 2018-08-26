65ba966652d640c687158d0d7e3e7e3cbcf24dff
Steven Inouye <steveninouye@msn.com>
2018-08-26 01:48:45 -1000

    completed get and post routes

--- server/src/routes/people.js
+++ server/src/routes/people.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import mysql from 'mysql';
 import {
     CreateChirp,
     DeleteChirp,
@@ -8,26 +9,75 @@ import {
 } from '../middleware/chirpstore';
 
 let router = Router();
+const connection = mysql.createConnection({
+    host: "localhost",
+    database: "chirpr",
+    user: 'chirprapp',
+    password: 'password'
+})
 
-router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    res.json(GetChirp(id));
-});
+router.get('/:id',(req,res) => {
+    const query = `SELECT * FROM users WHERE id=${req.params.id}`;
+    connection.connect();
+    connection.query(query, (err, result, fields) => {
+        if (err){
+            connection.end();
+            return console.log(err);
+        }
+        connection.end();
+        res.send(result);
+    })
+})
 
 router.get('/', (req, res) => {
-    res.json(GetChirps());
+    const query = `SELECT * FROM users;`
+    connection.connect();
+    connection.query(query, (err, result, fields) => {
+        if (err){
+            connection.end();
+            return console.log(err);
+        }
+        connection.end();
+        res.send(result);
+    })
 });
 
 router.post('/', (req, res) => {
-    CreateChirp(req.body);
-    res.send('hello from post route');
+    const query = `INSERT INTO users SET ?`;
+    const user = {
+        name: req.body.name,
+        age: req.body.age
+    }
+    connection.connect();
+    connection.query(query, user, (err, result, fields) => {
+        if (err){
+            connection.end();
+            return console.log(err);
+        }
+        connection.end();
+        res.send(result);
+    })
 });
 
-router.put('/', (req, res) => {
-    const id = parseInt(req.body.id, 10);
-    UpdateChirp(id, req.body);
-    res.send('hello from put route');
-});
+router.put('/:id',(req,res) => {
+    // const id = parseInt(req.body.id, 10);
+    // UpdateChirp(id, req.body);
+    // res.send('hello from put route');
+    const query = `UPDATE users SET ? WHERE id=${req.params.id}`;
+    const user = {
+        name: req.body.name,
+        age: req.body.age
+    }
+    connection.connect();
+    connection.query(query, user, (err, result, fields) => {
+        if (err){
+            connection.end();
+            return console.log(err);
+        }
+        connection.end();
+        res.send(result);
+    })
+})
 
 router.delete('/', (req, res) => {
     const id = parseInt(req.body.id, 10);
