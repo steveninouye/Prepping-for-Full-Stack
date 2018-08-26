66a4a37c3f70e2d2361c47431a908c7857da1489
Steven Inouye <steveninouye@msn.com>
2018-08-26 02:01:36 -1000

    completed PUT, DELETE, GET, & POST routes

--- server/src/routes/people.js
+++ server/src/routes/people.js
@@ -16,13 +16,13 @@ const connection = mysql.createConnection({
     password: 'password'
 })
 
-router.get('/:id',(req,res) => {
+router.get('/:id', (req, res) => {
     const query = `SELECT * FROM users WHERE id=${req.params.id}`;
     connection.connect();
     connection.query(query, (err, result, fields) => {
-        if (err){
+        if (err) {
             connection.end();
-            return console.log(err);
+            return console.log('ERROR: ', err);
         }
         connection.end();
         res.send(result);
@@ -33,9 +33,9 @@ router.get('/', (req, res) => {
     const query = `SELECT * FROM users;`
     connection.connect();
     connection.query(query, (err, result, fields) => {
-        if (err){
+        if (err) {
             connection.end();
-            return console.log(err);
+            return console.log('ERROR: ', err);
         }
         connection.end();
         res.send(result);
@@ -46,33 +46,33 @@ router.post('/', (req, res) => {
     const query = `INSERT INTO users SET ?`;
     const user = {
         name: req.body.name,
-        age: req.body.age
+        email: req.body.email,
+        password: req.body.password
     }
     connection.connect();
     connection.query(query, user, (err, result, fields) => {
-        if (err){
+        if (err) {
             connection.end();
-            return console.log(err);
+            return console.log('ERROR: ', err);
         }
         connection.end();
         res.send(result);
     })
 });
 
-router.put('/:id',(req,res) => {
-    // const id = parseInt(req.body.id, 10);
-    // UpdateChirp(id, req.body);
-    // res.send('hello from put route');
-    const query = `UPDATE users SET ? WHERE id=${req.params.id}`;
+router.put('/', (req, res) => {
+    const id = parseInt(req.body.id, 10);
+    const query = `UPDATE users SET ? WHERE id=${id}`;
     const user = {
         name: req.body.name,
-        age: req.body.age
+        email: req.body.email,
+        password: req.body.password
     }
     connection.connect();
     connection.query(query, user, (err, result, fields) => {
-        if (err){
+        if (err) {
             connection.end();
-            return console.log(err);
+            return console.log('ERROR: ', err);
         }
         connection.end();
         res.send(result);
@@ -81,8 +81,15 @@ router.put('/:id',(req,res) => {
 
 router.delete('/', (req, res) => {
     const id = parseInt(req.body.id, 10);
-    DeleteChirp(id);
-    res.send('hello from delete route');
+    const query = `DELETE FROM users WHERE id=${id}`;
+    connection.query(query, (err, result, fields) => {
+        if (err) {
+            connection.end();
+            return console.log('ERROR: ', err);
+        }
+        connection.end();
+        res.send(result);
+    })
 });
 
 export default router;
